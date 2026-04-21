23fa39a87fd236c34526535e29ac2ee74a61ff77
genissolercosta <genis.soler.costa@estudiantat.upc.edu>
2026-04-21 14:14:42 +0200

    segona versio

--- test_airports.py
+++ test_airports.py
@@ -1,5 +1,9 @@
 from airport import *
 
+airport = Airport ("LEBL", 41.297445, 2.0832941)
+SetSchengen(airport)
+PrintAirport (airport)
+
 airports = LoadAirports("Airports.txt")
 
 for airport in airports:
